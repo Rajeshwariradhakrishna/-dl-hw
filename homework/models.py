175c40ec91f722abd4874f3f5c498acc6960c154
Rajeshwari Radhakrishna <raji.nayak1989@gmail.com>
2025-03-18 20:05:59 -0500

    changes

--- homework/models.py
+++ homework/models.py
@@ -7,6 +7,48 @@ INPUT_MEAN = [0.2788, 0.2657, 0.2629]
 INPUT_STD = [0.2064, 0.1944, 0.2252]
 
 
+class Classifier(nn.Module):
+    def __init__(self, in_channels: int = 3, num_classes: int = 6):
+        super().__init__()
+        self.register_buffer("input_mean", torch.as_tensor(INPUT_MEAN))
+        self.register_buffer("input_std", torch.as_tensor(INPUT_STD))
+
+        # Convolutional Layers with BatchNorm and ReLU
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        # Fully connected layers for classification
+        self.fc1 = nn.Linear(128 * 8 * 8, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Normalize input
+        z = (x - self.input_mean[None, :, None, None]) / self.input_std[None, :, None, None]
+
+        # Convolutional layers with ReLU and BatchNorm
+        x = self.pool(torch.relu(self.bn1(self.conv1(z))))
+        x = self.pool(torch.relu(self.bn2(self.conv2(x))))
+        x = self.pool(torch.relu(self.bn3(self.conv3(x))))
+
+        # Flatten the output for fully connected layer
+        x = x.view(x.size(0), -1)
+
+        # Fully connected layers
+        x = torch.relu(self.fc1(x))
+        logits = self.fc2(x)
+
+        return logits
+
+    def predict(self, x: torch.Tensor) -> torch.Tensor:
+        return self(x).argmax(dim=1)
+
+
 class Detector(torch.nn.Module):
     def __init__(self, in_channels: int = 3, num_classes: int = 3):
         super().__init__()
@@ -16,14 +58,14 @@ class Detector(torch.nn.Module):
         # Encoder
         self.encoder1 = self._conv_block(in_channels, 32)  # (B, 32, H/2, W/2)
         self.encoder2 = self._conv_block(32, 64)          # (B, 64, H/4, W/4)
-        self.encoder3 = self._conv_block(64, 128)          # (B, 128, H/8, W/8)
-        self.encoder4 = self._conv_block(128, 256)         # (B, 256, H/16, W/16)
+        self.encoder3 = self._conv_block(64, 128)         # (B, 128, H/8, W/8)
+        self.encoder4 = self._conv_block(128, 256)        # (B, 256, H/16, W/16)
 
         # Decoder
-        self.decoder1 = self._upconv_block(256, 128)       # (B, 128, H/8, W/8)
-        self.decoder2 = self._upconv_block(128, 64)        # (B, 64, H/4, W/4)
-        self.decoder3 = self._upconv_block(64, 32)         # (B, 32, H/2, W/2)
-        self.decoder4 = self._upconv_block(32, 16)         # (B, 16, H, W)
+        self.decoder1 = self._upconv_block(256, 128)      # (B, 128, H/8, W/8)
+        self.decoder2 = self._upconv_block(128, 64)       # (B, 64, H/4, W/4)
+        self.decoder3 = self._upconv_block(64, 32)        # (B, 32, H/2, W/2)
+        self.decoder4 = self._upconv_block(32, 16)        # (B, 16, H, W)
 
         # Segmentation Head
         self.segmentation_conv = nn.Conv2d(16, num_classes, kernel_size=1)  # (B, num_classes, H, W)
@@ -74,7 +116,9 @@ class Detector(torch.nn.Module):
         depth = raw_depth.squeeze(1)  # (B, H, W)
         return pred, depth
 
+
 MODEL_FACTORY = {
+    "classifier": Classifier,
     "detector": Detector,
 }
 
